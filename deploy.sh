185f9e1a2e3b9a79306dac6d5b9f6f205d8bb22a
Gino Gravanis <ginogravanis@users.noreply.github.com>
2022-10-04 20:47:01 +0200

    Empty target folder before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-scp "$(dirname "$0")"/download.sh godmode:/var/www/install.godmode.sh/arch
+install_root="/var/www/install.godmode.sh"
+ssh godmode rm -rf "$install_root"/'*'
+scp "$(dirname "$0")/download.sh" godmode:"$install_root/arch"
